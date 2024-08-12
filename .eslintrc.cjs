6654e916079eec19d0556d279dc66c73b4b30d7e
sws <1141121512@qq.com>
2024-08-12 10:50:50 +0800

    1.shopxo-diy装修项目，代码引入 sws 2024-08-12

---
+++ .eslintrc.cjs
@@ -0,0 +1,48 @@
+// /* eslint-env node */
+// require('@rushstack/eslint-patch/modern-module-resolution')
+
+// module.exports = {
+//   root: true,
+//   'extends': [
+//     'plugin:vue/vue3-essential',
+//     'eslint:recommended',
+//     '@vue/eslint-config-typescript',
+//     '@vue/eslint-config-prettier/skip-formatting',
+// 	"./.eslintrc-auto-import.json"
+//   ],
+//   parserOptions: {
+//     ecmaVersion: 'latest'
+//   }
+// }
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    parser: "vue-eslint-parser",
+    extends: [
+      // 参考vuejs官方的eslint配置： https://eslint.vuejs.org/user-guide/#usage
+      "plugin:vue/vue3-recommended",
+      "./.eslintrc-auto-import.json",
+      "prettier",
+    ],
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: "@typescript-eslint/parser",
+    },
+    plugins: ["vue", "@typescript-eslint"],
+    rules: {
+      "vue/multi-word-component-names": "off", // 关闭组件名必须多字： https://eslint.vuejs.org/rules/multi-word-component-names.html
+      "@typescript-eslint/no-empty-function": "off", // 关闭空方法检查
+      "@typescript-eslint/no-explicit-any": "off", // 关闭any类型的警告
+      "vue/no-v-model-argument": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+    },
+    // https://eslint.org/docs/latest/use/configure/language-options#specifying-globals
+    globals: {
+      DialogOption: "readonly",
+      OptionType: "readonly",
+    },
+  };
