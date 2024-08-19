a04a96d68ec2e713ece399b5a07983e497f4415a
gongfuxiang <fuxiang.gong@qq.com>
2024-08-19 15:01:08 +0800

    login

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -30,30 +30,26 @@ service.interceptors.request.use(
 // 响应拦截器
 service.interceptors.response.use(
     (response: AxiosResponse) => {
-        const { code, msg } = response.data;
-        // 登录成功
-        if (code == '0') {
+        const { code, msg, data } = response.data;
+        if (code == 0) {
             return response.data;
+        } else if(code == -400) {
+            ElMessageBox.alert(msg, '温馨提示', {
+                confirmButtonText: '确定',
+                type: 'warning',
+            }).then(() => {
+                localStorage.clear(); // @vueuse/core 自动导入
+                window.location.href = data;
+            });
+        } else {
+            ElMessage.error(msg || '系统出错');
+            return Promise.reject(new Error(msg || 'Error'));
         }
-
-        ElMessage.error(msg || '系统出错');
-        return Promise.reject(new Error(msg || 'Error'));
     },
     (error: any) => {
         if (error.response.data) {
-            const { code, msg } = error.response.data;
-            // token 过期，跳转登录页
-            if (code === '-400') {
-                ElMessageBox.confirm('当前页面已失效，请重新登录', '提示', {
-                    confirmButtonText: '确定',
-                    type: 'warning',
-                }).then(() => {
-                    localStorage.clear(); // @vueuse/core 自动导入
-                    window.location.href = '/';
-                });
-            } else {
-                ElMessage.error(msg || '系统出错');
-            }
+            const { msg } = error.response.data;
+            ElMessage.error(msg || '系统出错');
         }
         return Promise.reject(error.message);
     }
