13c8bf4d7eacb0796cd466f6724ec7ffe81a3e42
于肖磊 <18851179580@163.com>
2024-08-23 18:51:41 +0800

    修改element-plus为中文

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ import router from './router';
 import Particles from 'vue3-particles';
 
 import common from '@/utils/common';
-import ElementPlus from 'element-plus';
-import zhCn from 'element-plus/es/locale/lang/zh-cn';
 //default styles
 import Vue3DraggableResizable from 'vue3-draggable-resizable'
 import 'vue3-draggable-resizable/dist/Vue3DraggableResizable.css'
@@ -21,9 +19,6 @@ app.config.globalProperties.$common = common;
 app.use(router);
 app.use(Particles);
 app.use(Vue3DraggableResizable);
-app.use(ElementPlus, {
-    locale: zhCn,
-});
 // 全局注册 状态管理(store)
 setupStore(app);
 
