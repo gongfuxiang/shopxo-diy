09c3a58bb083a23f355ae9115c906a0cc2735785
sws <1141121512@qq.com>
2024-08-29 14:56:26 +0800

    1.测试优化修改 sws 2024-08-28

--- src/components/model-search/model-search-content.vue
+++ src/components/model-search/model-search-content.vue
@@ -5,7 +5,7 @@
                 <div class="mb-12">展示设置</div>
                 <el-form-item label="图标样式" class="align-s">
                     <div class="flex-col w h gap-10">
-                        <el-switch v-model="form.is_icon_show"/>
+                        <el-switch v-model="form.is_icon_show" />
                         <template v-if="form.is_icon_show">
                             <upload v-model="form.icon_img_src" v-model:icon-value="form.icon_class" is-icon :limit="1" size="50"></upload>
                         </template>
@@ -13,16 +13,16 @@
                 </el-form-item>
                 <el-form-item label="提示文字">
                     <div class="flex-col gap-10 w">
-                        <el-switch v-model="form.is_tips_show"/>
+                        <el-switch v-model="form.is_tips_show" />
                         <el-input v-if="form.is_tips_show" v-model="form.tips" placeholder="请输入提示文字"></el-input>
                     </div>
                 </el-form-item>
                 <el-form-item v-if="form.is_tips_show || form.is_icon_show" label="是否居中">
-                    <el-switch v-model="form.is_center"/>
+                    <el-switch v-model="form.is_center" />
                 </el-form-item>
                 <el-form-item label="搜索按钮" class="align-s">
                     <div class="flex-col w h gap-10">
-                        <el-switch v-model="form.is_search_show"/>
+                        <el-switch v-model="form.is_search_show" />
                         <template v-if="form.is_search_show">
                             <el-radio-group v-model="form.search_type" class="ml-4">
                                 <el-radio value="img-icon">图片/图标</el-radio>
@@ -38,7 +38,7 @@
                     </div>
                 </el-form-item>
             </card-container>
-            <div class="bg-f5 partition-line" />
+            <div class="bg-f5 divider-line" />
             <card-container>
                 <div class="mb-12">搜索热词</div>
                 <drag :data="form.hot_word_list" @remove="remove" @on-sort="on_sort">
@@ -53,7 +53,7 @@
     </div>
 </template>
 <script setup lang="ts">
-import { get_math } from "@/utils";
+import { get_math } from '@/utils';
 interface Props {
     value: search_content;
 }
@@ -76,13 +76,13 @@ const props = withDefaults(defineProps<Props>(), {
                 id: get_math(), // 唯一标识使用，避免使用index作为唯一标识导致渲染节点出现问题
                 value: '',
                 color: '#000000',
-            }
+            },
         ],
-    })
+    }),
 });
 
 const state = reactive({
-    form: props.value
+    form: props.value,
 });
 // 如果需要解构，确保使用toRefs
 const { form } = toRefs(state);
@@ -100,14 +100,10 @@ const remove = (index: number) => {
 // 拖拽更新之后，更新数据
 const on_sort = (new_list: hot_word_list[]) => {
     form.value.hot_word_list = new_list;
-}
+};
 const search_color_change = (color: string, old_hot_word: hot_word_list) => {
-    const index = form.value.hot_word_list.findIndex(item => item.id == old_hot_word.id);
+    const index = form.value.hot_word_list.findIndex((item) => item.id == old_hot_word.id);
     (<arrayIndex>form.value.hot_word_list)[index].color = color;
-}
+};
 </script>
-<style lang="scss" scoped>
-.partition-line {
-    height: 0.8rem;
-}
-</style>
+<style lang="scss" scoped></style>
