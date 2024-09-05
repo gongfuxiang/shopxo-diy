713e8e61d4ddb0d84c38a599f2a4e5031b484428
sws <1141121512@qq.com>
2024-09-05 18:30:17 +0800

    开发分支中

--- temp.d.ts
+++
@@ -1,4 +0,0 @@
-const temp_token = 'e9448c2efc2bf9234aac50306aaaa5ab' || '';
-const temp_attachment_host = 'http://shopxo.com/public';
-// 导出token
-export default { temp_token, temp_attachment_host };
