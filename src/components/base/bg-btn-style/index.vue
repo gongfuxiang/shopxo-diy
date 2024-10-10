a54f0406d95a7e28036d6f6e46aa341d4d0886e7
sws <1141121512@qq.com>
2024-10-10 12:00:30 +0800

    1.icon更新

--- src/components/base/bg-btn-style/index.vue
+++ src/components/base/bg-btn-style/index.vue
@@ -1,7 +1,7 @@
 <template>
     <el-radio-group v-model="modelValue" is-button>
         <el-tooltip v-for="item in radio_list" :key="item.value" :content="item.name" placement="top" effect="light">
-            <el-radio-button :value="item.value"><icon name="spread-over"></icon></el-radio-button>
+            <el-radio-button :value="item.value"><icon :name="item.icon"></icon></el-radio-button>
         </el-tooltip>
     </el-radio-group>
 </template>
@@ -9,10 +9,10 @@
 <script setup lang="ts">
 const modelValue = defineModel({ type: String, default: '' });
 const radio_list = [
-    { name: '居上', value: '0' },
-    { name: '居下', value: '1' },
-    { name: '居中', value: '2' },
-    { name: '平铺', value: '3' },
-    { name: '铺满', value: '4' },
+    { name: '居上', value: '0', icon: 'up' },
+    { name: '居下', value: '1', icon: 'down' },
+    { name: '居中', value: '2', icon: 'middle' },
+    { name: '平铺', value: '3', icon: 'tile' },
+    { name: '铺满', value: '4', icon: 'spread-over' },
 ];
 </script>
