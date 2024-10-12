541de824ffe2f63bcbb8a04175a91aa64a6849d2
于肖磊 <18851179580@163.com>
2024-10-12 10:26:18 +0800

    修改提示显示

--- src/components/model-goods-tabs/model-goods-tabs-content.vue
+++ src/components/model-goods-tabs/model-goods-tabs-content.vue
@@ -7,7 +7,7 @@
                     <div class="flex-row align-c gap-10">
                         <el-switch v-model="form.tabs_top_up" active-value="1" inactive-value="0" :disabled="is_immersion_model"/>
                         <el-tooltip effect="dark" :show-after="200" :hide-after="200" content="<span>开启沉浸样式时，选项卡置顶功能禁用</span>" raw-content placement="top">
-                            <icon name="miaosha-hdgz" size="16"></icon>
+                            <icon name="miaosha-hdgz" size="12" color="#999"></icon>
                         </el-tooltip>
                     </div>
                 </el-form-item>
