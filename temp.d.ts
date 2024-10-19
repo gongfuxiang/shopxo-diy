eb261f61c42196b6c1e15eb44813d1d73e270e10
gongfuxiang <fuxiang.gong@qq.com>
2024-10-19 16:46:43 +0800

    temp

--- temp.d.ts
+++ temp.d.ts
@@ -1,4 +1,6 @@
+// 调试用户token
 const temp_token = '74be16979710d4c4e7c6647856088456' || '';
+// 调试接口地址
 const temp_attachment_host = 'http://shopxo.com';
 // 导出token
 export default { temp_token, temp_attachment_host };
