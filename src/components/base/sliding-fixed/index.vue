b44374e788b89c9858f9ebb1281788f4380d59f1
于肖磊 <18851179580@163.com>
2025-02-14 15:58:49 +0800

    描述修改

--- src/components/base/sliding-fixed/index.vue
+++ src/components/base/sliding-fixed/index.vue
@@ -2,7 +2,7 @@
     <el-form-item label="滑动固定" class="w mb-10">
         <div class="flex-row w h">
             <el-switch v-model="is_sliding_fixed" class="mr-10" active-value="1" inactive-value="0" @change="sliding_fixed_change" />
-            <el-tooltip effect="dark" :show-after="200" :hide-after="200" content="<span>1. 选项卡背景建议设置单纯色、多色或图片会造成背景不吻合。<br/> 2. 仅支持一个选项卡数据固定、开启后其他项将自动关闭。<br/>3.元素滑动到最左侧的时候固定</span>" raw-content placement="top">
+            <el-tooltip effect="dark" :show-after="200" :hide-after="200" content="<span>1. 选项卡背景建议设置单纯色、多色或图片会造成背景不吻合。<br/> 2. 仅支持一个选项卡数据固定、开启后其他项将自动关闭。<br/>3.元素滑动到最左侧的时候固定。</span>" raw-content placement="top">
                 <icon name="miaosha-hdgz" size="12" color="#999"></icon>
             </el-tooltip>
         </div>
