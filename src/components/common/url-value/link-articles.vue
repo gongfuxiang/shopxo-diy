bcc35246eff0e552746da1638786da2186c060e4
于肖磊 <18851179580@163.com>
2025-08-11 15:27:42 +0800

    修改调用地址

--- src/components/common/url-value/link-articles.vue
+++ src/components/common/url-value/link-articles.vue
@@ -111,7 +111,7 @@ const get_list = (new_page: number) => {
         page_size: page_size.value,
     };
     loading.value = true;
-   commonApi.getDynamicApi(props.linkUrl, new_data).then((res: any) => {
+    commonApi.getDynamicApi(props.linkUrl, new_data).then((res: any) => {
         tableData.value = res.data.data_list;
         data_total.value = res.data.data_total;
         page.value = res.data.page;
