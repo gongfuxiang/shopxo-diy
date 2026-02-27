12df80ee8868a9bdefbff07a804cbff37ee5bbfd
于肖磊 <18851179580@163.com>
2026-02-27 16:25:22 +0800

    新增短视频列表和短视频tabs

---
+++ src/api/pluginsVideo.ts
@@ -0,0 +1,21 @@
+import api_request from '@/utils/api-request';
+
+class PluginsVideoAPI {
+    /** 博客自动数据 */
+    static getAutoList(data: any) {
+        return api_request({
+            url: `plugins/index/pluginsname/pluginsVideo/pluginscontrol/diypluginsVideo/pluginsaction/autopluginsVideolist`,
+            method: 'post',
+            data,
+        });
+    }
+}
+
+export default PluginsVideoAPI;
+
+// 分类树结构
+
+export interface Tree {
+    /** 主键 */
+    id: string;
+}
